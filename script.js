35bf551b5307bc70d991e4e16a3b03ecae2247a1
Davíd Roggér <davidrogger.a@gmail.com>
2021-12-24 08:45:17 -0300

    Adicionado evento para os botoes de intro e sobre

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const displayedContent = document.getElementById('displayed-content-container');
 const imgContainer = document.getElementById('img-container');
 const nameContainer = document.getElementById('name-container');
-
+const aboutBtn = document.getElementById('about-btn');
+const introBtn = document.getElementById('intro-btn');
 
 function welcomeContent() {
   const welcomeText = '<h2> Bem vindo ao meu portfólio!</h2> <p> Nessa página você terá acesso aos conteúdos que estou estudando, projetos, redes sociais e também uma parte pessoal com relação aos hobbies quando não estou "codando" haha.🤖</p>'
@@ -15,9 +16,18 @@ function myName() {
 }
 
 function createNewImg(source) {
+  imgContainer.innerText = '';
   const newImg = document.createElement('img');
   newImg.src = source;
   imgContainer.appendChild(newImg);
 }
 
+function aboutDisplay() {
+  const aboutTxt = '<h2>Um pouco sobre mim</h2> <p>Meu nome é Davíd Roggér, sou <strong>Brasileiro🇧🇷</strong>, do interior do estado de São Paulo, cidade chamada <a target="_blank" href="https://pt.wikipedia.org/wiki/Boituva">Boituva</a>, conhecida por ser a capital do paraquedismo.'
+  displayedContent.innerHTML = aboutTxt;
+  createNewImg('./img/dvd-firstday.jpg');
+}
+
+aboutBtn.addEventListener('click', aboutDisplay);
+introBtn.addEventListener('click', welcomeContent);
 window.onload = welcomeContent(), myName();
