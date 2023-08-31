5f0ff60d573371f993631ec40236737308c1435c
davidrogger <davidrogger.a@gmail.com>
2023-08-31 09:19:45 -0300

    BUILD: install react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import LayoutRoot from '@/components/LayoutRoot';
-
 import './index.css';
 
 import App from './App';
 
+const homeElements = (
+  <LayoutRoot>
+    <App />
+  </LayoutRoot>
+);
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: homeElements,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LayoutRoot>
-      <App />
-    </LayoutRoot>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
