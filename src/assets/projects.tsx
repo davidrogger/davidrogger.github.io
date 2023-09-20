42b977177ce39d940478299640fb3f8f5f09766e
davidrogger <davidrogger.a@gmail.com>
2023-09-20 16:18:32 -0300

    FEAT: add projects

--- src/assets/projects.tsx
+++ src/assets/projects.tsx
@@ -1,17 +1,120 @@
-export const projects = [
+const frontend = [
   {
     title: 'Solar System',
+    type: 'frontend',
     img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-solar-system/main/imgs/project_solar_system_top.jpg',
     link: 'https://github.com/davidrogger/trybe-project-solar-system'
   },
   {
     title: 'Tryunfo',
+    type: 'frontend',
     img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-tryunfo/main/imgs/project_tryunfo_top.webp',
     link: 'https://github.com/davidrogger/trybe-project-tryunfo'
   },
   {
-    title: 'Solar System',
-    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-solar-system/main/imgs/project_solar_system_top.jpg',
-    link: 'https://github.com/davidrogger/trybe-project-solar-system'
+    title: 'Trybetunes',
+    type: 'frontend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-trybetunes/main/readme-imgs/project_trybetunes_mid.webp',
+    link: 'https://github.com/davidrogger/trybe-project-trybetunes'
   },
+  {
+    title: 'Online Store',
+    type: 'frontend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-frontend-online-store/main/readme-imgs/project_frontend_store_top.webp',
+    link: 'https://github.com/davidrogger/trybe-project-frontend-online-store'
+  },
+  {
+    title: 'TrybeWallet',
+    type: 'frontend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-trybewallet/main/readme-imgs/project_trybewallet_top.webp',
+    link: 'https://github.com/davidrogger/trybe-project-trybewallet'
+  },
+  {
+    title: 'Trivia',
+    type: 'frontend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-trivia/main/readme-imgs/project_trivia_top.webp',
+    link: 'https://github.com/davidrogger/trybe-project-trivia'
+  },
+  {
+    title: 'StarWars Planets Search',
+    type: 'frontend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-starwars-planets-search/main/readme-imgs/project_starwars_planets_top.webp',
+    link: 'https://github.com/davidrogger/trybe-project-starwars-planets-search'
+  },
+  {
+    title: 'Recipes App',
+    type: 'frontend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-recipes-app/main/readme-imgs/project_recipes_app_top.webp',
+    link: 'https://github.com/davidrogger/trybe-project-recipes-app'
+  },
+];
+
+const testing = [
+  {
+    title: 'RTL',
+    type: 'Testing',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-react-testing-library/main/readme-imgs/project_rtl_top2.webp',
+    link: 'https://github.com/davidrogger/trybe-project-react-testing-library'
+  },
+];
+
+const backend = [
+  {
+    title: 'Talker Manager',
+    type: 'backend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-backend/main/imgs/04_project_talker_manager.webp',
+    link: 'https://github.com/davidrogger/trybe-project-talker-manager'
+  },
+  {
+    title: 'Store Manager',
+    type: 'backend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-backend/main/imgs/05_project_store_manager.webp',
+    link: 'https://github.com/davidrogger/trybe-project-store-manager'
+  },
+  {
+    title: 'Blogs API',
+    type: 'backend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-backend/main/imgs/06_project_blogs_api.webp',
+    link: 'https://github.com/davidrogger/trybe-project-blogs-api'
+  },
+  {
+    title: 'Trybesmith',
+    type: 'backend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-backend/main/imgs/07_project_trybesmith.webp',
+    link: 'https://github.com/davidrogger/trybe-project-trybesmith'
+  },
+  {
+    title: 'Trybe Futebol Club',
+    type: 'backend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-futebol-club/main/readme-imgs/project_top.webp',
+    link: 'https://github.com/davidrogger/trybe-project-futebol-club'
+  },
+  {
+    title: 'Car Shop',
+    type: 'backend',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-backend/main/imgs/11_project_trybe_car_shop.webp',
+    link: 'https://github.com/davidrogger/trybe-project-car-shop'
+  },
+];
+
+const fullstack = [
+  {
+    title: 'Delivery App',
+    type: 'fullstack',
+    img: 'https://raw.githubusercontent.com/davidrogger/trybe-project-delivery-app/main/readme-imgs/project_fe_mid.webp',
+    link: 'https://github.com/davidrogger/trybe-project-delivery-app'
+  },
+  {
+    title: 'Talker Manager II',
+    type: 'fullstack',
+    img: 'https://img.youtube.com/vi/5j2_DD0e708/0.jpg',
+    link: 'https://github.com/davidrogger/talker-manager-ts'
+  },
+];
+
+export const projects = [
+  ...fullstack,
+  ...backend,
+  ...frontend,
+  ...testing,
 ];
--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -9,7 +9,7 @@ type ProjectCardProps = {
 export function ProjectCard({ title, img, link }:ProjectCardProps) {
   return (
     <div
-      className='border h-60 w-56 rounded-xl overflow-hidden hover:shadow-neutral-400 relative'
+      className='border h-48 w-56 opacity-80 hover:opacity-100 rounded-xl overflow-hidden hover:shadow-neutral-400 relative'
     >
       <Link
         to={link}
@@ -22,7 +22,7 @@ export function ProjectCard({ title, img, link }:ProjectCardProps) {
         />
       </Link>
       
-      <div className='bg-primary h-20 absolute inset-x-0 bottom-0'>
+      <div className='bg-primary h-10 absolute inset-x-0 bottom-0'>
         <h1
           className='text p-2 w-full h-full flex justify-center items-end'
         >
